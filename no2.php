461a75ea321ddd61708f47f955edc294b36d41f4
Nexxy <124580974+Vandifrs@users.noreply.github.com>
2023-02-07 10:15:08 +0700

    Add files via upload

---
+++ no2.php
@@ -0,0 +1,5 @@
+<?php   
+    $strin = "ababab";  
+    $sub_str = "aba";  
+    echo substr_count($strin, $sub_str);  
+?>    
---
+++ no3.php
@@ -0,0 +1,17 @@
+<?php
+// PHP program to demonstrate the 
+// substr_count() function
+   
+// $start is passed
+$str = "Hello World"; 
+  
+echo "[d] = ", substr_count($str, "d") , " ,";
+echo "[e] = ", substr_count($str, "e") , " ,";;  
+echo "[H] = ", substr_count($str, "H") , " ,";;  
+echo "[l] = ", substr_count($str, "l") , " ,";;    
+echo "[o] = ", substr_count($str, "o") , " ,";;  
+echo "[r] = ", substr_count($str, "r") , " ,";;  
+echo "[W] = ", substr_count($str, "W");  
+
+  
+?>
